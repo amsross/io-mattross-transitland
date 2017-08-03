1bd9b18aed42670f258b6b9cd7ba0c873afd9acb
Matt Ross <amsross@gmail.com>
2017-08-03 18:00:52 -0400

    Remove logging and correctly respond

--- handlers/index.js
+++ handlers/index.js
@@ -8,7 +8,7 @@ const operators = require('./operators')
 const stops = require('./stops')
 const schedules = require('./schedules')
 
-module.exports.next = function next(req, params) {
+module.exports.next = function next(params) {
 
   const options = {}
   const fuseConfig = {
@@ -17,7 +17,6 @@ module.exports.next = function next(req, params) {
       { name: 'trip_headsign', weight: 0.7 }
     ]
   }
-  if (req && req.log && req.log.info) req.log.info(params)
 
   return operators(options)(params.ON)
     .flatMap(operator => stops(options)(params.FROM)(operator)
--- index.js
+++ index.js
@@ -2,20 +2,11 @@
 const r = require('ramda')
 const express = require('express')
 const alexa = require('alexa-app')
-const bunyan = require('bunyan')
-const bunyanMiddleware = require('bunyan-middleware')
 const handlers = require('./handlers')
 
 const app = express()
 const alexaApp = new alexa.app(process.env.APP_NAME || 'alexa');
-const logger = bunyan.createLogger({name: process.env.APP_NAME || 'alexa'})
 
-app.use(bunyanMiddleware({
-  headerName: 'X-Request-Id',
-  propertyName: 'reqId',
-  logName: 'req_id',
-  logger: logger,
-}))
 app.set('view engine', 'ejs')
 alexaApp.express({
   endpoint: 'alexa',
@@ -35,34 +26,32 @@ alexaApp.intent('trainIntent', {
     'next train on {-|ON} from {-|FROM}',
     'next train on {-|ON} from {-|FROM} to {-|TO}'
   ]
-}, (req, res) => handlers.next(req, {
+}, (req, res) => new Promise((resolve, reject) => handlers.next({
   ON: req.slot('ON'),
   FROM: req.slot('FROM'),
   TO: req.slot('TO')
 })
-  .collect()
   .toCallback((err, results) => {
     if (err) return res.say(err.message)
     return r.compose(
-      x => res.say(x),
+      r.tap(() => resolve()),
+      r.tap(x => res.say(x)),
       obj => `The next trains on ${obj.operator_name} from ${obj.stop_name}` +
         (req.slot('TO') ? ` to ${req.slot('TO')}` : ``) +
         ` are ${obj.schedules}`,
       r.evolve({
         schedules: r.compose(
-          r.map(schedule => `${schedule.origin_departure_time} to ${schedule.trip_headsign}`),
-          r.join(', '))
+          r.join(', '),
+          r.map(schedule => `${schedule.origin_departure_time} to ${schedule.trip_headsign}`))
       }),
       r.identity)(results)
-  }))
+  })))
 
-app.get('/next/:ON/:FROM/:TO', (req, res, next) => handlers.next(req, req.params)
-  .collect()
+app.get('/next/:ON/:FROM/:TO', (req, res, next) => handlers.next(req.params)
   .tap(json => res.json(json))
   .toCallback(err => next(err)))
 
-app.get('/next/:ON/:FROM', (req, res, next) => handlers.next(req, req.params)
-  .collect()
+app.get('/next/:ON/:FROM', (req, res, next) => handlers.next(req.params)
   .tap(json => res.json(json))
   .toCallback(err => next(err)))
 
