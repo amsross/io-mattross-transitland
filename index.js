eee36da1c1a790f19eda30b2ef1867477577118d
Matt Ross <amsross@gmail.com>
2017-08-03 16:47:27 -0400

    Add alexa-app and an intent

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
+'use strict'
+const r = require('ramda')
 const express = require('express')
+const alexa = require('alexa-app')
 const handlers = require('./handlers')
 
 const app = express()
+const alexaApp = new alexa.app(process.env.APP_NAME || 'alexa');
 
-app.get('/next/:on/:from/:to', respond(handlers.next))
-app.get('/next/:on/:from', respond(handlers.next))
+app.set('view engine', 'ejs')
+alexaApp.express({
+  endpoint: 'alexa',
+  expressApp: app,
+  checkCert: process.env.ENV === 'production' ? true : false,
+  debug: process.env.ENV === 'production' ? false : true,
+})
+
+alexaApp.launch((req, res) => response.say('Try asking about the next train on an operator from a station.'))
+alexaApp.intent('trainIntent', {
+  'slots': {
+    'ON': 'LITERAL',
+    'FROM': 'LITERAL',
+    'TO': 'LITERAL'
+  },
+  'utterances': [
+    'next train on {-|ON} from {-|FROM}',
+    'next train on {-|ON} from {-|FROM} to {-|TO}'
+  ]
+}, (req, res) => handlers.next({
+  ON: req.slot('ON'),
+  FROM: req.slot('FROM'),
+  TO: req.slot('TO')
+})
+  .collect()
+  .toCallback((err, res) => {
+    if (err) return res.say(err.message)
+    return r.compose(
+      x => res.say(x),
+      obj => return `The next trains on ${obj.operator_name} from ${obj.stop_name}` +
+        (req.slot('TO') ? ` to ${req.slot('TO')}` : ``) +
+        ` are ${obj.schedules}`,
+      r.evolve({
+        schedules: r.compose(
+          r.map(schedule => `${schedule.origin_departure_time} to ${schedule.trip_headsign}`),
+          r.join(', '))
+      }),
+      r.identity)(res)
+  }))
+
 app.get('/next/:on/:from/:to', (req, res, next) => handlers.next(req.params)
   .collect()
   .tap(json => res.json(json))
